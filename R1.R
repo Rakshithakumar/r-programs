fa899fdc6915a27dfd66004e11cd00bbb09f4218
Rakshitha K <rakshithakumar50@gmail.com>
2022-08-16 23:05:08 +0530

    R file

---
+++ R1.R
@@ -0,0 +1,29 @@
+getwd()
+
+message <- "Hello World"
+print(message)
+
+a <- 10
+print(a)
+
+#load the data
+data(iris)
+iris
+
+tail(iris)
+
+
+names(iris)
+
+min(iris$Sepal.Length)
+
+max(iris$Sepal.Length)
+
+mean(iris$Petal.Width)
+
+
+quantile(iris$Sepal.Length)
+
+mean(iris$Sepal.Length)
+
+summary(iris)
